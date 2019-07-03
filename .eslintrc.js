215e5222a8a48bf2ade391b3759e328f28170083
ctong5 <cassandra.tong5@gmail.com>
2019-07-02 19:05:43 -0700

    Update README description and eslint style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,24 @@
  *   https://github.com/reactorcore/eslint-config-hackreactor
  */
 
+// module.exports = {
+//   extends: './node_modules/eslint-config-hackreactor/index.js'
+// };
+
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  extend: 'hackreactor',
+  rules: {
+    // Rules here will override the 'hackreactor' configuration: http://eslint.org/docs/rules/
+  }
 };
+
+/*
+* Now, you can run ESLint from the command line:
+* eslint [options] [file|dir|glob]
+*
+* For example:
+* eslint someFile.js someOtherFile.js
+* eslint client/**
+* If you don't see any output, your files have passed all the linting rules. In addition to the command line interface, ESLint can be integrated into
+* various build systems like Gulp, Grunt, or a pre-commit hook.
+*/
