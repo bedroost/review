20469b7398cceb8c746cf8a5f62ffe70468f72d5
ctong5 <cassandra.tong5@gmail.com>
2019-07-02 23:28:10 -0700

    Test webpack with bare minimum App component and index.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <div>
+      {"I'm alive!"}
+    </div>
+  );
+
+}
+
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<p>I'm alive!</p>, document.getElementById('app'));
