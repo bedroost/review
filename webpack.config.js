392456be40141901f1b0921fa9dbaa64689fb864
ctong5 <cassandra.tong5@gmail.com>
2019-07-02 23:27:00 -0700

    Build webpack to bundle and transpile JS files for usage in browser

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  mode: 'development',
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+};
