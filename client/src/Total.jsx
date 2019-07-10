9433fedd78ad3ac06c752d3a42ec4527dc98d302
ctong5 <cassandra.tong5@gmail.com>
2019-07-10 01:13:33 -0700

    Pass props into Total component. Render data into rough styling

--- client/src/Total.jsx
+++ client/src/Total.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Total = props => (
   <div className="total-stars">
 
-    <div>
-      <span id="total-no-reviews">{`${props.reviews.length} Reviews`}</span>
+    <div className="total-count-reviews">
+      <span>{`${props.reviews.length} Reviews`}</span>
     </div>
 
-    <div className="stars">
-      <span>{props.ratings.overall}</span>
+    <div>
+      <span className="total-overall-stars">{props.ratings.overall}</span>
     </div>
 
   </div>
