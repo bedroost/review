2bf44228bd192000338b92e89529a90cdd7a8ddb
ctong5 <cassandra.tong5@gmail.com>
2019-07-12 02:24:50 -0700

    Add PageButtons rendered case for currentPage 4

--- client/src/Pages.jsx
+++ client/src/Pages.jsx
@@ -53,6 +53,20 @@ const Pages = (props) => {
           <PageForward />
         </div>
       );
+    } else if (currentPage === 4) {
+      pagesDisplayed = (
+        <div className="pagesList">
+          <PageBack />
+          <PageButton handlePageClick={handlePageClick} currentPage={currentPage - 3} />
+          <PageButton handlePageClick={handlePageClick} currentPage={currentPage - 2} />
+          <PageButton handlePageClick={handlePageClick} currentPage={currentPage - 1} />
+          <PageButton handlePageClick={handlePageClick} currentPage={currentPage} />
+          <PageButton handlePageClick={handlePageClick} currentPage={currentPage + 1} />
+          <li><div>...</div></li>
+          <PageButton handlePageClick={handlePageClick} currentPage={n} />
+          <PageForward />
+        </div>
+      );
     } else {
       pagesDisplayed = (
         <div>
