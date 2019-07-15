c60e78e62b924eb1c0af205b62b4fcf04728fc25
ctong5 <cassandra.tong5@gmail.com>
2019-07-15 00:57:22 -0700

    Change totalReviews default from null to 0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      totalReviews: null,
+      totalReviews: 0,
       allReviews: [],
       allSearchedReviews: null,
       displayedReviews: [],
