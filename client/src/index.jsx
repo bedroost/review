d49033023f23461cc894fd76868869308ea159ac
ctong5 <cassandra.tong5@gmail.com>
2019-07-15 17:34:39 -0700

    Update target id location according to team naming convention (rootModule)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import '../dist/style.css';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('rootReview'));
