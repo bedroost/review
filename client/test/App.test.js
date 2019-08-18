9cbcd18d54616cff89b1f7bd577045ef8f58fb80
ctong5 <cassandra.tong5@gmail.com>
2019-08-18 15:07:44 -0700

    Remove console logs and add new tests

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -2,21 +2,26 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import App from '../src/App';
-import Ratings from '../src/Ratings';
+import RatingsAccuracy from '../src/RatingsAccuracy';
+import RatingsCheckIn from '../src/RatingsCheckIn';
+import RatingsCleanliness from '../src/RatingsCleanliness';
+import RatingsCommunication from '../src/RatingsCommunication';
+import RatingsLocation from '../src/RatingsLocation';
+import RatingsValue from '../src/RatingsValue';
 import ReviewList from '../src/ReviewList';
 import ReviewListItem from '../src/ReviewListItem';
+import Search from '../src/Search';
 import Pages from '../src/Pages';
+import PageBack from '../src/PageBack';
+const mockAdapter = new MockAdapter(axios);
 
 describe('<App /> rendering', () => {
   const wrapper = shallow(<App />);
 
   it('should render <App /> component', () => {
     expect(wrapper.exists()).toBe(true);
-  });
-
-  it('should render without throwing an error', () => {
-    expect(wrapper.find('.reviewApp').exists()).toBe(true);
   });
 
   it('should render <Total /> component', () => {
@@ -50,54 +55,120 @@ describe('<App /> rendering', () => {
   it('should have an initial state ratings default to empty object', () => {
     expect(wrapper.state('ratings')).toBeInstanceOf(Object);
   });
+
+  it('should call getData when component mounts', (done) => {
+    wrapper.instance().getData = jest.fn();
+    wrapper.update();
+    wrapper.instance().componentDidMount();
+    expect(wrapper.instance().getData).toBeCalled();
+    done();
+  });
+
+  it('should have an inital state totalReviews default to 0', () => {
+    expect(wrapper.state('totalReviews')).toEqual(0);
+  });
 });
 
-/*
-Add tests below when jest mock axios is available
-*/
+describe('<Search /> rendering', () => {
+  const wrapper = shallow(<Search />);
 
-// jest.mock('axios');
-// it('should have an inital state totalReviews default to null', () => {
-//   expect(wrapper.state('totalReviews')).toBeNull();
-// });
-//
-// describe('<ReviewList /> rendering', () => {
-//   it('should render without throwing an error', () => {
-//     const wrapper = shallow(<ReviewList />);
-//     expect(wrapper.contains(<div>Reviews</div>)).toBe(true);
-//   });
+  it('should have an input field', () => {
+    expect(wrapper.containsMatchingElement(<input type="text" />)).toBeTruthy();
+  });
 
-//   it('should render individual reviews', () => {
-//     const wrapper = shallow(<ReviewList />);
-//     expect(wrapper.contains('div')).toHaveLength(3);
-//   });
+  it('should have an keypress event on \'Enter\'', () => {
+    expect(wrapper.find('input').simulate('keydown', { keyCode: 13 }));
+  });
 
-//   it('should render seven <ReviewListItem /> components', () => {
-//     const wrapper = shallow(<ReviewList />);
-//     expect(wrapper.children('<ReviewListItem />')).toHaveLength(7);
-//   });
-// });
+  it('should have an image in search bar called \'search\'', () => {
+    expect(wrapper.containsMatchingElement(<i>search</i>)).toBeTruthy();
+  });
 
-// describe('ReviewListItem /> rendering', () => {
+  // test onChange methods
+  // it('should have an input field with placeholder text \'Search reviews\'', () => {
+  //   expect(wrapper.find('input').simulate('change', { target: { value: 'hello' } }));
+  // });
+});
+
+
+describe('<RatingsAccuracy /> rendering', () => {
+  const wrapper = shallow(<RatingsAccuracy />);
+
+  it('should have a rating represented by UTF8 star', () => {
+    expect(wrapper.containsMatchingElement(<span>★</span>)).toBeTruthy();
+  });
+
+  it('should render 11 individual spans', () => {
+    expect(wrapper.find('span')).toHaveLength(11);
+  });
+});
+
+describe('<RatingsCheckIn /> rendering', () => {
+  const wrapper = shallow(<RatingsCheckIn />);
+
+  it('should have a rating represented by UTF8 star', () => {
+    expect(wrapper.containsMatchingElement(<span>★</span>)).toBeTruthy();
+  });
+
+  it('should render 11 individual spans', () => {
+    expect(wrapper.find('span')).toHaveLength(11);
+  });
+});
+
+describe('<RatingsCleanliness /> rendering', () => {
+  const wrapper = shallow(<RatingsCleanliness />);
+
+  it('should have a rating represented by UTF8 star', () => {
+    expect(wrapper.containsMatchingElement(<span>★</span>)).toBeTruthy();
+  });
+
+  it('should render 11 individual spans', () => {
+    expect(wrapper.find('span')).toHaveLength(11);
+  });
+});
+
+describe('<RatingsCommunication /> rendering', () => {
+  const wrapper = shallow(<RatingsCommunication />);
+
+  it('should have a rating represented by UTF8 star', () => {
+    expect(wrapper.containsMatchingElement(<span>★</span>)).toBeTruthy();
+  });
+
+  it('should render 11 individual spans', () => {
+    expect(wrapper.find('span')).toHaveLength(11);
+  });
+});
+
+describe('<RatingsLocation /> rendering', () => {
+  const wrapper = shallow(<RatingsLocation />);
+
+  it('should have a rating represented by UTF8 star', () => {
+    expect(wrapper.containsMatchingElement(<span>★</span>)).toBeTruthy();
+  });
+
+  it('should render 11 individual spans', () => {
+    expect(wrapper.find('span')).toHaveLength(11);
+  });
+});
+
+describe('<RatingsValue /> rendering', () => {
+  const wrapper = shallow(<RatingsValue />);
+
+  it('should have a rating represented by UTF8 star', () => {
+    expect(wrapper.containsMatchingElement(<span>★</span>)).toBeTruthy();
+  });
+
+  it('should render 11 individual spans', () => {
+    expect(wrapper.find('span')).toHaveLength(11);
+  });
+});
+
+// describe('<ReviewListItem /> rendering', () => {
+//   const avatar = 'https://s3.amazonaws.com/uifaces/faces/twitter/dixchen/128.jpg';
+//   const text = 'Harum id in est ut repellat odio dolorem et explicabo. Minus delectus modi veritatis. Perferendis ex fugiat.';
+
 //   it('renders an <img>', () => {
-//     const wrapper = shallow(<ReviewListItem />);
-//     expect(wrapper.find('img')).to.have.lengthOf(1);
-//   });
-// });
-
-// describe('<Ratings /> rendering', () => {
-//   it('should render <App /> component', () => {
-//     const wrapper = shallow(<Ratings />);
-//     expect(wrapper.exists()).toBe(true);
-//   });
-
-//   it('should render without throwing an error', () => {
-//     const wrapper = shallow(<Ratings />);
-//     expect(wrapper.find('.reviewApp').exists()).toBe(true);
-//   });
-
-//   it('should render two Ratings Wells', () => {
-//     const wrapper = shallow(<Ratings />);
-//     expect(wrapper.find('.well')).toHaveLength(2);
+//     const wrapper = shallow(<ReviewListItem avatar={avatar} text={text}/>);
+//     expect(wrapper.containsMatchingElement(<img src={avatar} alt="" />)).toBeTruthy();
 //   });
 // });
